9538c74ad46b6f1c46f4062258f44ab6349c5f92
Germain Morilhat <germain@MacBook-Pro-de-Germain.local>
2022-01-21 14:02:19 +0100

    v2

--- home/views.py
+++ home/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from .models import Article,Sport
 
 # Create your views here.
-def home(request):
-    return render(request,'home/home.html')
 
 def article_id(request,id):
     article=Article.objects.get(id=id)
@@ -12,3 +10,20 @@ def article_id(request,id):
 def sports(request):
     sport_data=Sport.objects.all()   
     return render(request,'home/sports.html',{'sports':sport_data})
+
+def home(request):
+    articles_data=Article.objects.all()
+    nbr_articles=len(articles_data)
+    last_article=Article.objects.get(id=nbr_articles)
+    data={}
+    data['last_article']=last_article
+    print(data,nbr_articles)
+    for i in range(1,4):
+        print(i)
+        try:
+            data[str(i)]=Article.objects.get(id=nbr_articles-i)
+        except:
+            print("L'article n'existe pas")
+        else:
+            pass
+    return render(request,'home/home.html',data)
